099e2d5dc9044080f675b6616b11950c671f0e8b
IBM_SAYEM <ibmcsecu@gmail.com>
2022-12-08 21:15:39 -0500

    Update 03_face_recognition.py

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -1,63 +1,76 @@
 import cv2
 import numpy as np
-import os 
+import os
+import time
+from mail import sendEmail
 
+# main.check_for_objects()
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.xml')
+recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# iniciate id counter
 id = 0
 
 # names related to ids: example ==> ibmsayem: id=1,  etc
-names = ['None', 'ibmsayem', 'abbu', 'mama']
+names = ['None', 'sayem', 'naeem']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
 # Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
 
 while True:
 
-    ret, img =cam.read()
+    ret, img = cam.read()
     # img = cv2.flip(img, -1) # Flip vertically
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale( 
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-    for(x,y,w,h) in faces:
+    for (x, y, w, h) in faces:
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(img, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 4)
 
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        print confidence
 
-        # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if confidence < 100:
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
             id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
+            # when i found an unknown face a timer will be created and between this timer i send mail
 
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+            confidence = "  {0}%".format(round(100 - confidence))
+            cv2.imwrite("unknown/Unknown." + str(y) + ".jpg", gray[y:y + h, x:x + w])
+            sendEmail("unknown/Unknown." + str(y) + ".jpg")
+            print('email successfully send!!')
+            # exec "mail.py"
+            # time.sleep(5000)
+
+        # cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(id), (x, y - 40), font, 2, (255, 255, 255), 3)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
 
